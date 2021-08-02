416c56eaae8b875837c8e2007154f6f32a91c328
heada13 <babolatlist_1003@yahoo.co.jp>
2021-08-02 17:40:33 +0900

    api疎通

---
+++ back/pyapi/index.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request, render_template
+from pycoingecko import CoinGeckoAPI
+
+app = Flask(__name__, static_folder='../../front/dist/static', template_folder='../../front/dist')
+# app = Flask(__name__)
+
+incomes = [
+  { 'description': 'salary', 'amount': 5000 }
+]
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return render_template('index.html')
+
+@app.route('/incomes')
+def get_incomes():
+  return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+  incomes.append(request.get_json())
+  return '', 204
+
+@app.route('/main',methods=['GET'])
+def get_coin():
+    cg = CoinGeckoAPI()
+    # cg.ping()
+    print("run bitcoin")
+    bitcoin = cg.get_price(ids='bitcoin', vs_currencies='usd')
+    return bitcoin
--- front/src/components/Price.vue
+++ front/src/components/Price.vue
@@ -11,12 +11,28 @@
               市況
           </span>
           <span class="el_marketPriceValue">{{marketPrice}}</span>
+          <span>{{tableData}}</span>
       </div>
   </div>
 </template>
 
 <script>
+// const axiosBase = require('axios').create()
+// import axios from 'axios'
+// const axios = axiosBase({
+//   baseURL: 'http://127.0.0.1:5000/', // バックエンドB のURL:port を指定する
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'X-Requested-With': 'XMLHttpRequest'
+//   },
+//   responseType: 'json'  
+// }); 
 export default {
+    data () {
+        return {
+        tableData: null
+        }
+    },
     props:{
         basePriceValue:{
             type:String,
@@ -34,6 +50,17 @@ export default {
         marketPrice(){
             return this.marketPriceValue
         }
+    },
+    mounted () {
+        this.updataTableData()
+    },
+    methods: {
+        updataTableData: function () {
+            this.$axios.get('/main')
+            .then(response => (this.tableData = response.data))
+            .catch(error => console.log(error))
+        }
+            // this.tableData = response.data
     }
 
 }
